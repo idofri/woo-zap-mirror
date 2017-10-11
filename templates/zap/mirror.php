4bc09409c9dd41f455eabfab0a12034858a2ac00
1do <idofri@gmail.com>
2017-10-11 17:25:29 +0300

    use plugin methods in case of overloading

--- templates/zap/mirror.php
+++ templates/zap/mirror.php
@@ -26,14 +26,14 @@ $nodes = ob_get_clean();
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<title><?php wp_title(''); ?></title>
-	<link rel="stylesheet" href="<?php echo plugins_url( '/assets/dtree/dtree.css', dirname( __DIR__ ) ); ?>" type="text/css" />
-	<script type="text/javascript" src="<?php echo plugins_url( '/assets/dtree/dtree.js', dirname( __DIR__ ) ); ?>"></script>
+	<link rel="stylesheet" href="<?php echo WC_Zap_Mirror()->plugin_url(); ?>/assets/dtree/dtree.css" type="text/css" />
+	<script type="text/javascript" src="<?php echo WC_Zap_Mirror()->plugin_url(); ?>/assets/dtree/dtree.js"></script>
 </head>
 <body <?php body_class(); ?>>
 	<script type="text/javascript">
 		d = new dTree('d');
 		for (var key in d.icon) {
-			d.icon[key] = '<?php echo plugins_url( '/assets/dtree/', dirname( __DIR__ ) ); ?>' + d.icon[key];
+			d.icon[key] = '<?php echo WC_Zap_Mirror()->plugin_url(); ?>/assets/dtree/' + d.icon[key];
 		}
 		d.add(0, -1, '<?php echo get_bloginfo( 'name' ); ?>', '<?php echo wc_get_page_permalink( 'zap' ); ?>');
 		<?php echo $nodes; ?>
