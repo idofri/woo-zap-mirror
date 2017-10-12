66d91d624f952169771e83cc9abebae7d1cd621a
1do <idofri@gmail.com>
2017-10-12 13:35:38 +0300

    return instead of exiting

--- WC_Zap_Mirror.php
+++ WC_Zap_Mirror.php
@@ -193,7 +193,7 @@ final class WC_Zap_Mirror {
 	public function template_loader( $template ) {
 		if (  is_page( wc_get_page_id( 'zap' ) ) ) {
 			$this->mirror_site();
-			exit;
+			return;
 		}
 		return $template;
 	}
