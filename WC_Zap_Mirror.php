68cc3abecbd6f67d5ffe4b4ab905385d7979420f
Ido Friedlander <ido@imark.co.il>
2017-11-28 15:40:54 +0200

    exit instead of return

--- WC_Zap_Mirror.php
+++ WC_Zap_Mirror.php
@@ -209,7 +209,7 @@ final class WC_Zap_Mirror {
 	public function template_loader( $template ) {
 		if ( is_page( wc_get_page_id( 'zap' ) ) ) {
 			$this->mirror_site();
-			return;
+			exit;
 		}
 		return $template;
 	}
